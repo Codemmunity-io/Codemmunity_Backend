4d2acf15db1dbc1a3a9eba003d12a0339b86a867
MaheshNat <maheshkumar.natamai@gmail.com>
2021-02-16 20:25:43 -0600

    added boilerplate for sequelize and auth/posts routes

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
-const app = express(); 
-// environmental vars - make a file called .env and then put "PORT=(whatever port u want)" in it (without the quotes)
-const port = process.env.PORT; 
-const mysql = require('mysql');
+const db = require('./models');
 
-// haven't created any credentials for the database yet
-// this is just to get a boilerplate set up
-// we might want a connection pool rather than 1 connection further down the line
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME1
-})
+const app = express();
+const authRouter = require('./routes/auth');
+const postsRouter = require('./routes/posts');
 
+const PORT = process.env.PORT || 8080;
+
+app.use(express.json());
+app.use('/api/auth', authRouter);
+app.use('/api/posts', postsRouter);
 app.get('/', (req, res) => {
-    res.send('Hello world!').status(200);
-})
+  res.send('Hello world!').status(200);
+});
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-})
+const init = async () => {
+  await db.sequelize.sync();
+  app.listen(PORT, () => {
+    console.log(`App listening at http://localhost:${PORT}`);
+  });
+};
+
+init();
---
+++ models/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const db = {};
+
+let sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach((file) => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach((modelName) => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
